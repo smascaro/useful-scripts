d6e66a9523118d41ce193690cdf1b3b543b42961
Sergi <sergi@MacBookPro.home>
2025-01-08 22:39:34 +0100

    Add useful git aliases

---
+++ git_aliases.sh
@@ -0,0 +1,4 @@
+git config --global alias.logg '!git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit'
+git config --global alias.unstage '!git restore --staged .'
+git config --global alias.ss '!git status --short'
+git config --global alias.cu '!git reset --hard'
